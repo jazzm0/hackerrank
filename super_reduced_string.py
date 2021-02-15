9ef3b23790dcb098d9473881a7623aaf40785aff
Tibor Tarnai <tibor.tarnai@sap.com>
2021-02-15 20:11:12 +0100

    reduced string

--- super_reduced_string.py
+++ super_reduced_string.py
@@ -5,19 +5,10 @@ def superReducedString(s):
     i = 0
     while i < len(s) - 1:
         if s[i] == s[i + 1]:
-            s = s.replace(s[i] + s[i + 1], '')
-            if i > 0:
-                i -= 2
-            elif i == 0:
-                i = -1
+            s = s[:i] + s[i + 2:]
+            i = max(i - 2, -1)
 
         i += 1
-
-    if len(s) == 0:
-        print('Empty String')
-        return 'Empty String'
-
-    print(s)
     return s
 
 
@@ -27,15 +18,15 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(superReducedString('aaabccddd'), 'abd')
 
     def test_b(self):
-        self.assertEqual(superReducedString('aa'), 'Empty String')
+        self.assertEqual(superReducedString('aa'), '')
 
     def test_c(self):
-        self.assertEqual(superReducedString('baab'), 'Empty String')
+        self.assertEqual(superReducedString('baab'), '')
 
     def test_d(self):
         self.assertEqual(superReducedString(
             'ppffccmmssnnhhbbmmggxxaaooeeqqeennffzzaaeeyyaaggggeessvvssggbbccnnrrjjxxuuzzbbjjrruuaaccaaoommkkkkxx'),
-                         'Empty String')
+            '')
 
 
 if __name__ == '__main__':
