48cfe541cbdf9b4e87a9ce6adadb1661e169213a
d050150 <tibor.tarnai@sap.com>
2022-01-21 15:33:23 +0100

    love letter

--- love_letter.py
+++ love_letter.py
@@ -6,8 +6,8 @@ import unittest
 def theLoveLetterMystery(s):
     count = 0
     for i in range(len(s) // 2):
-        if s[i] != s[-i - 1]:
-            count += abs(ord(s[i]) - ord(s[-i - 1]))
+        if s[i] != s[-(i + 1)]:
+            count += abs(ord(s[i]) - ord(s[-(i + 1)]))
     return count
 
 
