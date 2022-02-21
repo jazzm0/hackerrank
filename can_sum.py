78210894d389a3dc6e933da5db0af580338c692c
d050150 <tibor.tarnai@sap.com>
2022-02-21 11:20:51 +0100

    improve can/best sum

--- can_sum.py
+++ can_sum.py
@@ -1,15 +1,11 @@
 import unittest
 
 
-def can_sum(target, coins, memo=None, solutions=None, depth=None):
+def can_sum(target, coins, memo=None):
     if memo is None:
         memo = {}
 
-    if depth is None:
-        depth = 0
-
-    if solutions is None:
-        solutions = {}
+    best_solution = None
 
     if target == 0:
         return []
@@ -22,22 +18,15 @@ def can_sum(target, coins, memo=None, solutions=None, depth=None):
 
     for i in range(len(coins)):
         if coins[i] <= target:
-            result = can_sum(target - coins[i], coins, memo, solutions, depth + 1)
+            result = can_sum(target - coins[i], coins, memo)
             if result is not None:
                 result.append(coins[i])
-                if depth == 0:
-                    same_length = solutions.get(len(result), list())
-                    same_length.append(result)
-                    solutions[len(result)] = same_length
-                else:
-                    return result
+                if best_solution is None or len(result) < len(best_solution):
+                    best_solution = result
             else:
                 memo[target] = None
 
-    if len(solutions) == 0:
-        return None
-    else:
-        return solutions[sorted(solutions.keys())[0]][0]
+    return best_solution
 
 
 class TestStringMethods(unittest.TestCase):
@@ -46,7 +35,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(can_sum(1, [1, 2, 5]), [1])
 
     def test_b(self):
-        self.assertEqual(can_sum(3, [1, 2, 5]), [1, 2])
+        self.assertEqual(can_sum(3, [1, 2, 5]), [2, 1])
 
     def test_c(self):
         self.assertEqual(can_sum(3, [2, 1, 5]), [1, 2])
