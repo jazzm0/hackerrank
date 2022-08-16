d40bb23f7bc046510a9d2b0e9bdf352f41072938
d050150 <tibor.tarnai@sap.com>
2022-08-16 12:12:16 +0200

    maximize stock

---
+++ stock_maximize.py
@@ -0,0 +1,38 @@
+import sys
+import unittest
+
+# https://www.hackerrank.com/challenges/stockmax/
+
+sys.setrecursionlimit(5000)
+
+
+def stockmax(prices, day=0, share_count=0, profit=0, cost=0):
+    if day == (len(prices) - 1):
+        return profit + share_count * prices[day] - cost
+
+    profit = max(
+        max(stockmax(prices, day + 1, share_count + 1, profit, cost + prices[day]),
+            stockmax(prices, day + 1, 0, profit + share_count * prices[day], cost)),
+        stockmax(prices, day + 1, share_count, profit, cost))
+    
+    return profit
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_a(self):
+        self.assertEqual(stockmax([5, 3, 2]), 0)
+
+    def test_b(self):
+        self.assertEqual(stockmax([1, 2, 100]), 197)
+
+    def test_c(self):
+        self.assertEqual(stockmax([1, 3, 1, 2]), 3)
+
+    def test_d(self):
+        self.assertEqual(stockmax(
+            [38430, 35991, 26818, 19088, 37402, 82378]), 254161)
+
+
+if __name__ == '__main__':
+    unittest.main()
