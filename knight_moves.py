a188e930e45199e154f3690e25d4c080d0a4808a
d050150 <tibor.tarnai@sap.com>
2022-10-26 11:17:56 +0200

    knight moves

--- knight_moves.py
+++ knight_moves.py
@@ -1,58 +1,73 @@
 import unittest
 
 
-class Cell:
-    def __init__(self, x=0, y=0, dist=0):
-        self.x = x
-        self.y = y
-        self.dist = dist
+# https://www.hackerrank.com/challenges/knightl-on-chessboard
+
+def knight(a, b, location, n):
+    next_locations = set()
+    if location[0] + a < n and location[1] + b < n:
+        next_locations.add((location[0] + a, location[1] + b))
+        next_locations.add((location[1] + b, location[0] + a))
+
+    if location[0] + a < n and location[1] - b >= 0:
+        next_locations.add((location[0] + a, location[1] - b))
+        next_locations.add((location[1] - b, location[0] + a))
+
+    if location[0] - a >= 0 and location[1] + b < n:
+        next_locations.add((location[0] - a, location[1] + b))
+        next_locations.add((location[1] + b, location[0] - a))
+
+    if location[0] - a >= 0 and location[1] - b >= 0:
+        next_locations.add((location[0] - a, location[1] - b))
+        next_locations.add((location[1] - b, location[0] - a))
+
+    return next_locations
 
 
-def is_inside(x, y):
-    if 0 <= x <= 7 and 0 <= y <= 7:
-        return True
-    return False
+def knightlOnAChessboard(n):
+    start = (0, 0)
+    end = (n - 1, n - 1)
+    overall_result = []
+    for a in range(1, n):
+        result = []
+        for b in range(1, n):
+            visited_locations = {start}
+            new_locations = knight(a, b, start, n)
+            length = 1
+            while len(new_locations) != 0:
+                all_new_locations = set()
+                for location in new_locations:
+                    visited_locations.add(location)
+                    temp_new_locations = knight(a, b, location, n)
+                    for temp in temp_new_locations:
+                        if temp not in visited_locations:
+                            all_new_locations.add(temp)
 
+                if end in visited_locations:
+                    result.append(length)
+                    break
 
-def solution(knightpos, targetpos):
-    d = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+                if len(all_new_locations) == 0:
+                    result.append(-1)
+                    break
 
-    knightpos = (knightpos // 8, knightpos % 8)
-    targetpos = (targetpos // 8, targetpos % 8)
+                length += 1
+                new_locations = all_new_locations
 
-    queue = [Cell(knightpos[0], knightpos[1], 0)]
-
-    visited = [[False for _ in range(8)]
-               for _ in range(8)]
-
-    visited[knightpos[0]][knightpos[1]] = True
-
-    while len(queue) > 0:
-
-        t = queue[0]
-        queue.pop(0)
-
-        if (t.x == targetpos[0] and
-                t.y == targetpos[1]):
-            return t.dist
-
-        for i in range(8):
-
-            x = t.x + d[i][0]
-            y = t.y + d[i][1]
-
-            if is_inside(x, y) and not visited[x][y]:
-                visited[x][y] = True
-                queue.append(Cell(x, y, t.dist + 1))
+        overall_result.append(result)
+    return overall_result
 
 
 class TestStringMethods(unittest.TestCase):
 
     def test_a(self):
-        self.assertEqual(solution(0, 1), 3)
+        self.assertEqual(knightlOnAChessboard(5), [[4, 4, 2, 8],
+                                                   [4, 2, 4, 4],
+                                                   [2, 4, -1, -1],
+                                                   [8, 4, -1, 1]])
 
     def test_b(self):
-        self.assertEqual(solution(19, 36), 1)
+        self.assertEqual(knight(1, 2, (2, 2), 5), {(0, 1), (3, 4), (4, 3), (0, 3), (1, 4), (3, 0), (1, 0), (4, 1)})
 
 
 if __name__ == '__main__':
