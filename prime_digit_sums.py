4d5b2bfe3c9780dd5ec5aa80c325e8bc41db207d
d050150 <tibor.tarnai@sap.com>
2023-09-12 18:39:32 +0200

    improve performance

--- prime_digit_sums.py
+++ prime_digit_sums.py
@@ -1,6 +1,6 @@
+import sys
 import unittest
 from math import sqrt
-import sys
 
 sys.set_int_max_str_digits(0)
 
@@ -24,18 +24,52 @@ for i in range(1, 50):
     if is_prime(i):
         primes.add(i)
 
+three_digits = set()
+for i in range(1, 1000):
+    digits = str(i)
+    while len(digits) < 3:
+        digits = "0" + digits
+    number = int(digits[0]) + int(digits[1]) + int(digits[2])
+    if number in primes:
+        three_digits.add(digits)
+
+four_digits = set()
+for i in range(1, 10000):
+    digits = str(i)
+    while len(digits) < 4:
+        digits = "0" + digits
+    number = int(digits[0]) + int(digits[1]) + int(digits[2]) + int(digits[3])
+    if number in primes:
+        four_digits.add(digits)
+
+
+five_digits = set()
+for i in range(1, 100000):
+    digits = str(i)
+    while len(digits) < 5:
+        digits = "0" + digits
+    number = (
+        int(digits[0])
+        + int(digits[1])
+        + int(digits[2])
+        + int(digits[3])
+        + int(digits[4])
+    )
+    if number in primes:
+        five_digits.add(digits)
+
 
 def is_composite_prime(n):
     digits = str(n)
     for i in range(len(digits) - 2):
-        number = int(digits[i]) + int(digits[i + 1]) + int(digits[i + 2])
-        if number in primes:
+        number = digits[i] + digits[i + 1] + digits[i + 2]
+        if number in three_digits:
             if i < len(digits) - 3:
-                number += int(digits[i + 3])
-                if number in primes:
+                number += digits[i + 3]
+                if number in four_digits:
                     if i < len(digits) - 4:
-                        number += int(digits[i + 4])
-                        if number not in primes:
+                        number += digits[i + 4]
+                        if number not in five_digits:
                             return False
                 else:
                     return False
@@ -68,7 +102,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(primeDigitSums(3), 303)
 
     def test_d(self):
-        self.assertEqual(primeDigitSums(3691), 303)
+        self.assertEqual(primeDigitSums(8), 295)
 
 
 if __name__ == "__main__":
